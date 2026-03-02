797c51cd635106e1234fbdd944caf44ae1cdfe82
Axel <axelfercharra@gmail.com>
2026-03-02 20:39:28 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if 'logeado' not in st.session_state or not st.session_state['logeado']:
                     # MAGIA: Usamos UNA SOLA cookie que cumple ambas funciones
                     cookie_manager.set("usuario_finanzas", usuario, expires_at=vencimiento) 
                     
-                    st.success(f"¡Bienvenido/a {usuario}! cargando tus finanzas...")
+                    st.success(f"¡Bienvenido/a {usuario}! Cargando tus finanzas...")
                     time.sleep(1.5)
                     st.rerun()
                 else:
